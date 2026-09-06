95e748af34ad350c60b25ca4d421916c81a947ec
Carlos Berdejo <carlos_berdejo@hotmail.com>
2026-09-06 17:06:15 -0300

    test(qa): add intentional review findings

---
+++ qa/fixtures/ReviewQaFixture.groovy
@@ -0,0 +1,20 @@
+package qa.fixtures
+
+import java.io.File
+
+class ReviewQaFixture {
+
+    static String loadName(Map<String, String> input) {
+        String name = input.get('name')
+        name.toUpperCase()
+    }
+
+    static String execute(String command) {
+        Runtime.runtime.exec(command)
+    }
+
+    static String readConfig(File file) {
+        FileInputStream stream = new FileInputStream(file)
+        stream.text
+    }
+}
